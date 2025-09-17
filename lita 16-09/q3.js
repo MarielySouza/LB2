14d1e65f60aaf182fed8e26437189c0eb46d68ea
MarielySouza <mariely29042009@gmail.com>
2025-09-17 14:42:53 -0300

    Add files via upload

---
+++ lita 16-09/q3.js
@@ -0,0 +1,12 @@
+`faça a leitura de uma fita de DNA de 10 elementos, depois faça o complemento de leitura
+dessa fita e imprima os elementos na sequencia correta`
+
+const DNA = []
+for(i = 0; i < 10; i++){
+    DNA[i] = prompt("Escreva o elemento da fita")
+}
+
+DNA.forEach((item, index) => {
+  
+  )  
+})
