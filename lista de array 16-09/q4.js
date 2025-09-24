6e60db18eb26e2d63f95f6173a7fad4b2cbf3921
MarielySouza <mariely29042009@gmail.com>
2025-09-24 14:49:06 -0300

    Add files via upload

---
+++ lista de array 16-09/q4.js
@@ -0,0 +1,51 @@
+`um aluno deve preencher as notas trimestrais referentes a cada uma de suas 8 materias, cada matéria deve ter
+nome, nota e frequencia. Preencha os dados e posteriormente:
+
+    a)imprima a média de frequencia de todas as diciplinas
+    b)imprima a quantidade de notas acima da media (60%)
+`
+let acimamedia = 0
+const materias = []
+const notas = []
+const frequencia= []
+
+for(i = 0; i < 8; i++){
+
+    materias[i] = prompt("Qual o nome da matéria?")
+
+    notas[i] = prompt("Qual sua nota na matéria " +materias[i])
+    
+
+    frequencia[i] = prompt("Qual sua frequencia na materia " +materias[i])
+
+    if(notas[i] >= 21){
+        acimamedia++
+    }
+
+}
+
+let frequenciaf = 0
+
+for(f = 0; f < 8; f++){
+
+    frequenciaf += frequencia[f]
+
+}
+
+console.log(frequenciaf)
+
+
+
+
+
+
+
+
+
+
+
+console.log(materias)
+console.log(notas)
+console.log(frequencia)
+console.log("A quantidade de notas acima da média é: " +acimamedia);
+
